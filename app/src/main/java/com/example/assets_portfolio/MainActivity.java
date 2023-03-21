908cd579a991b285f6ad1136a61cf71b97fde7e1
jk1 <jakub.kos212@gmail.com>
2023-03-22 00:35:34 +0100

    added new xml - dialog.xml deleted previous xmls

--- app/src/main/java/com/example/assets_portfolio/MainActivity.java
+++ app/src/main/java/com/example/assets_portfolio/MainActivity.java
@@ -1,30 +1,83 @@
 package com.example.assets_portfolio;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    AlertDialog dialog;
+    LinearLayout layout;
 
+    Button add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        add = findViewById(R.id.addViewButton);
+        layout = findViewById(R.id.categoryLinearLayout);
+
+        buildDialog();
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.show();
+            }
+        });
+
+
     }
 
-    public void add(View v){
-        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    public void categoryAlertDialog(View v){
+        dialog.show();
+    }
 
-        LinearLayout linearLayout = findViewById(R.id.categoryLinearLayout);
-        View view = inflater.inflate(R.layout.category, linearLayout);
-        linearLayout.addView(view);
+    private void buildDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.category, null);
 
+        EditText name = view.findViewById(R.id.name);
 
+        builder.setView(view);
+        builder.setTitle("Podaj Nazwe").setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                addCard(name.getText().toString());
+            }
+        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+                dialog = builder.create();
+    }
+
+    private void addCard(String name){
+        View view = getLayoutInflater().inflate(R.layout.category, null);
+
+        TextView nameView = view.findViewById(R.id.name);
+        Button delete = view.findViewById(R.id.delete);
+
+        nameView.setText(name);
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout.removeView(view);
+            }
+        });
+        layout.addView(view);
     }
 
 
